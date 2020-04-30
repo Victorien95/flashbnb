56d0fffb98a2962152216108966875fec1809eeb
STD <victorien.ifocop@gmail.com>
2020-04-30 17:37:28 +0200

    feat: Authentifaction utilisateurs + formulaires de connexion/mdp ...

---
+++ src/Form/ApplicationType.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+
+class ApplicationType extends AbstractType
+{
+    /**
+     * Permet d'avoir la configuration de base d'un champ
+     * @param string $placeholder
+     * @param array $options
+     * @return array
+     */
+    protected function getConfiguration($placeholder, $options = ['required' => true]){
+        return array_merge([
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
+}
